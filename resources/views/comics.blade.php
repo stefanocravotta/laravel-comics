c2fe07e2adc85b6b9a31086c897ee21cd3099554
stefanocravotta <you@example.com>
2022-06-23 16:54:11 +0200

    $data

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -8,14 +8,18 @@
     <div class="container card-container">
 
       <button class="btn">CURRENT SERIES</button>
-      
+        
+        @foreach ($comics as $comic)
+            
         <div class="card">
             <div class="container-img">
-                <img src="comic.thumb" alt="comic.type">
+                <img src="{{ $comic->image }}" alt="{{ $comic->type }}">
             </div>
             
-            <h3>comic.series</h3>
+            <h3>{{ $comic->title }}</h3>
         </div>
+
+        @endforeach
 
     </div>
     <button class="btn-load">LOAD MORE</button>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/comics', function () {
-    return view('comics');
+
+    $data = config('comics');
+
+    return view('comics', ['comics' => $data]);
 })->name('comics');
