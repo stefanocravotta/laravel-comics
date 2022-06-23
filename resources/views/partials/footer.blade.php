1b3f39706810e4b228cc2bc53580407bcaa94b55
stefanocravotta <you@example.com>
2022-06-23 17:13:27 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,113 @@
 <footer>
-    ciao F
+    <div class="container footer-main clearfix">
+        <div class="cols link-cols">
+            <div class="list-link">
+                <h3>DC Comics</h3>
+                <ul>
+                      <li>
+                          <a href="">Characters</a>
+                      </li>
+                      <li>
+                          <a href="">Comics</a>
+                      </li>
+                      <li>
+                          <a href="">Movies</a>
+                      </li>
+                      <li>
+                          <a href="">TV</a>
+                      </li>
+                      <li>
+                          <a href="">Games</a>
+                      </li>
+                      <li>
+                          <a href="">Videos</a>
+                      </li>
+                      <li>
+                          <a href="">News</a>
+                      </li>
+                </ul>
+                <h3>Shop</h3>
+                <ul>
+                      <li>
+                          <a href="">Shop DC</a>
+                      </li>
+                      <li>
+                          <a href="">Shop DC Collectibles</a>
+                      </li>
+                </ul>
+            </div>
+            <div class="list-link">
+                <h3>DC</h3>
+                <ul>
+                      <li>
+                          <a href="">Terms Of Use</a>
+                      </li>
+                      <li>
+                          <a href="">Privacy policy(New)</a>
+                      </li>
+                      <li>
+                          <a href="">Ad Choices</a>
+                      </li>
+                      <li>
+                          <a href="">Advertising</a>
+                      </li>
+                      <li>
+                          <a href="">Jobs</a>
+                      </li>
+                      <li>
+                          <a href="">Subscriptions</a>
+                      </li>
+                      <li>
+                          <a href="">Talent Workshops</a>
+                      </li>
+                      <li>
+                          <a href="">CPSC Certificatess</a>
+                      </li>
+                      <li>
+                          <a href="">Ratingss</a>
+                      </li>
+                      <li>
+                          <a href="">Shop Helps</a>
+                      </li>
+                      <li>
+                          <a href="">Contact Us</a>
+                      </li>
+                </ul>
+            </div>
+            <div class="list-link">
+                <h3>Sites</h3>
+                <ul>
+                      <li>
+                          <a href="">DC</a>
+                      </li>
+                      <li>
+                          <a href="">MAD Magazine</a>
+                      </li>
+                      <li>
+                          <a href="">DC Kids</a>
+                      </li>
+                      <li>
+                          <a href="">DC Universe</a>
+                      </li>
+                      <li>
+                          <a href="">DC Power Visa</a>
+                      </li>
+                </ul>
+            </div>
+        </div>
+        <div class="cols image"></div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container h-100">
+              <button class="btn">SIGN-UP NOW!</button>
+              <div class="social">
+                  <h3>FOLLOW US</h3>
+                  <a href=""><img src="../img/footer-facebook.png" alt=""></a>
+                  <a href=""><img src="../img/footer-twitter.png" alt=""></a>
+                  <a href=""><img src="../img/footer-youtube.png" alt=""></a>
+                  <a href=""><img src="../img/footer-pinterest.png" alt=""></a>
+                  <a href=""><img src="../img/footer-periscope.png" alt=""></a>
+              </div>
+        </div>
+    </div>
 </footer>
