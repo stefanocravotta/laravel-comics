57ad142f7482d8f1af2475da1a801b81ab235ec5
stefanocravotta <you@example.com>
2022-06-23 17:03:29 +0200

    icon section

--- resources/views/partials/icon.blade.php
+++ resources/views/partials/icon.blade.php
@@ -1,1 +1,26 @@
-<h1>Ciao icon</h1>
+<div id="icon-box">
+    <div class="h-100 container">
+        <ul class="h-100">
+          <li>
+              <img src="../img/buy-comics-digital-comics.png" alt="Digital Comics">
+              <a href="#">Digital Comics</a>
+          </li>
+          <li>
+              <img src="../img/buy-comics-merchandise.png" alt="Digital Comics">
+              <a href="#">DC Merchandise</a>
+          </li>
+          <li>
+              <img src="../img/buy-comics-subscriptions.png" alt="Digital Comics">
+              <a href="#">Subscription</a>
+          </li>
+          <li>
+              <img src="../img/buy-comics-shop-locator.png" alt="Digital Comics">
+              <a href="#">Comic Shop Locator</a>
+          </li>
+          <li>
+              <img src="../img/buy-dc-power-visa.svg" alt="Digital Comics">
+              <a href="#">DC Power visas</a>
+          </li>
+        </ul>
+    </div>
+</div>
